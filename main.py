7423fb79c03e042287ae8fa24bfb82d127da01b3
dsneddon00 <dsneddon00@gmail.com>
2021-10-10 20:15:33 -0600

    added some baseline functions, need more info on how to proceed

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+import math
+import random
+
+def main():
 
 
 
+
+def monteCarlo(num):
+    # Implement (in Python) a function that takes an integer as input and returns True or False as output,
+    #indicating whether or not the integer is prime. Use the Monte Carlo probabalistic algorithm discussed
+    #in section 7.2, so that it will finish in reasonable time given several hundred digit test numbers.
+    #We will use this piece of code later when we implement RSA encription.
+
+    # Hint: Replace the Python command:
+    # remainder = b ** e % n
+    # with the equivalent but far faster:
+    # remainder = pow(b,e,n)
+
+
+def generateNumberRandom(min, max):
+    return random.randrange(min, max)
+
+
+if __name__ == "__main__":
+    main()
 
 # Notes from class - 10/1/2021
 # IsPrimeMiller(n):
