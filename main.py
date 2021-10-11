0a9f83affa491e500eb6e506378c84b6b6bfd7eb
Derek Sneddon <dsneddon00@gmail.com>
2021-10-11 10:50:57 -0600

    added some notes

--- main.py
+++ main.py
@@ -50,3 +50,46 @@ if __name__ == "__main__":
 # b^(T) % n is 1    2^(25) % 101 is 1 ? no (10)
 # j = 0 b^(2*T) % n is (n-1)    2^(25) % 101 is 100? no
 # j = 1     2^50 % 101 is 100? yes
+
+# Notes from class - 10/11/2021
+# Refer to pg 463
+# Refer to pg 464 example 16
+# Monte Carlo algorithm is a chance algorithm
+# code example:
+
+# (This is the monteCarlo part of the algorithm)
+# def IsPrimeMiller(n):
+# for i in range(20):
+#   probablyPrime = MillersAlgorithm(N)
+#   if not probablyPrime:
+#       return False # For sure composite
+# return True # almost surely prime
+
+# if n = 11, n-1 = 10 = 2*5, 2'*5, s=1, T = 5
+# in this case b is between 2 and 10 inclusive
+# b = 10
+# b^T % n == 1
+# 10^5 % 11
+
+
+# Another test case
+# n = 161
+# to find if its prime, try everything up to the square root of it
+# n-1 = 160
+#       80
+#       40
+#       20
+#       10
+#       5
+#       s = 5, T = 5
+# b is between 2 and 160, 75
+# is b^T % n == 1?
+# is 75^5 % 161 == 1? (it's 94, so we haven't passed Miller's test yet)
+# J tests
+# if it passes 0, 1, 2, 3, 4 then it is good enough
+# b^((2^J)(T))
+# if j = 0, returns 94 No
+# if j = 1, returns 142 No
+# if j = 2, returns 39 No
+# if j = 3, returns 72 No
+# if j = 4, returns 32, No
